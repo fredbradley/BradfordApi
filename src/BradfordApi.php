811d6a473950d21e718cf5c3f002d01e7acc0a51
Fred Bradley <hello@fredbradley.co.uk>
2020-06-25 16:04:52 +0100

    Update

--- src/BradfordApi.php
+++ src/BradfordApi.php
@@ -2,7 +2,50 @@
 
 namespace FredBradley\BradfordApi;
 
+use GuzzleHttp\Client;
+
 class BradfordApi
 {
-    // Build your next great package.
+    protected $client;
+
+    public function __construct(string $url, string $username, string $password)
+    {
+        $this->client = new Client([
+            'base_uri' => $url,
+            'auth' => [
+                $username,
+                $password
+            ],
+            'headers' => [
+                'Accept' => 'application/json'
+            ]
+        ]);
+    }
+
+    /**
+     * Shorthand function to create requests with JSON body and query parameters.
+     * @param $method
+     * @param string $uri
+     * @param array $json
+     * @param array $query
+     * @param array $options
+     * @param boolean $decode JSON decode response body (defaults to true).
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function request(
+        $method,
+        $uri = '',
+        array $json = [],
+        array $query = [],
+        array $options = [],
+        $decode = true
+    ) {
+        $response = $this->client->request($method, $uri, array_merge([
+            'json' => $json,
+            'query' => $query
+        ], $options));
+
+        return $decode ? json_decode((string)$response->getBody(), true) : (string)$response->getBody();
+    }
 }
